e1a171060ce15e5015495bb290fc22c82f84389c
aasamta <aasamt@outlook.com>
2017-03-17 16:55:13 -0400

    btn3

--- SE_Master_Utility/SE_Master_Utility/Form1.Designer.cs
+++ SE_Master_Utility/SE_Master_Utility/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.lb_SWComps = new System.Windows.Forms.ListBox();
             this.lb_SWObjects = new System.Windows.Forms.ListBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -84,6 +85,15 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(642, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 66);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -91,6 +101,7 @@
             this.AutoScroll = true;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(2137, 1047);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.lb_SWObjects);
             this.Controls.Add(this.lb_SWComps);
             this.Controls.Add(this.button2);
@@ -110,6 +121,7 @@
         private System.Windows.Forms.ListBox lb_SWComps;
         private System.Windows.Forms.ListBox lb_SWObjects;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
