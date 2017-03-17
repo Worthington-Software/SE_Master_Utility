9b96d0af5be1c25f202f81bff73041e2c4e68f49
aasamta <aasamt@outlook.com>
2017-03-17 16:38:49 -0400

    Jibs 2

--- SE_Master_Utility/SE_Master_Utility/Form1.Designer.cs
+++ SE_Master_Utility/SE_Master_Utility/Form1.Designer.cs
@@ -43,7 +43,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(500, 101);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Load Test Values Jibberish";
+            this.button1.Text = "Load Test Values Jibberish 11";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
